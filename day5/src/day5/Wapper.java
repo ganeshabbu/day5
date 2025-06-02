fa46b1872f9e999e767cc005fbc42a3f9007c1ce
ganeshabbu <108935028+ganeshabbu@users.noreply.github.com>
2025-06-02 10:41:34 +0530

    commit

---
+++ day5/src/day5/Wapper.java
@@ -0,0 +1,17 @@
+package day5;
+
+public class Wapper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1;
+		Integer b = 1;
+		
+		double price = 99.99;
+		Double price2 = 99.99;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
